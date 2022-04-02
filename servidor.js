c8823d93f4a8d930baa38832f5747223b98dbdc0
Mateus Gomes Veloso - MGE <mateus.gomes@iteris.com.br>
2022-04-02 20:58:09 -0300

    Terminando o endpoint post e criando o endpoint put e delete

--- servidor.js
+++ servidor.js
@@ -10,19 +10,20 @@ for(let i = 0; i < clientesNames.length; i++){
 
 const http = require("http");
 const express = require("express");
-const bp = require('body-parser')
+const bp = require('body-parser');
+const e = require("express");
 
 const app = express();
 
 app.use(bp.json())
 app.use(bp.urlencoded({ extended: true }))
 
-//Mostra todas as pessoas
+//Mostra todos os clientes
 app.get("/clientes", function(req, res) {
     res.json(clientesArray);
 });
 
-//Mostra uma pessoa em específico
+//Mostra um cliente em específico
 app.get('/cliente/:id', function(req, res) {
     let clienteEcontrada = clientesArray.find(function (item){
         return item.id === parseInt(req.params.id);
@@ -31,20 +32,61 @@ app.get('/cliente/:id', function(req, res) {
     res.json(clienteEcontrada);
 });
 
+//Cadastra um cliente
 app.post('/cliente/cadastrarnovocliente', function(req, res){
     let itemIds = clientesArray.map(item => item.id);
 
     let newId = itemIds.length > 0 ? Math.max.apply(Math, itemIds) + 1 : 1;
-   
-    let newItem = {
+
+    let newcliente = {
         id: newId, 
         name: req.body.name
     };
 
-    clientesArray.push(newItem); 
+    clientesArray.push(newcliente); 
 
- res.json(newItem);
+ res.json(newcliente);
 
+});
+
+//Atualiza um cliente
+app.put('/cliente/editarcliente/:id', function (req, res) {
+   
+    let clienteEncontrado = clientesArray.find(function (item) {
+        return item.id === parseInt(req.params.id);
+    });
+
+    if (clienteEncontrado) {
+        let clienteAtualizado = {
+            id: clienteEncontrado.id,
+            title: req.body.name,
+        }; 
+
+        let index = clientesArray.indexOf(clienteEncontrado);
+
+        clientesArray.splice(index, 1, clienteAtualizado);
+
+        res.json(clienteAtualizado);
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+//Deleta um cliente
+app.delete('/cliente/deletarcliente/:id', function (req, res) {
+
+    let clienteEcontrado = clientesArray.find(function (item) {
+        return item.id === parseInt(req.params.id);
+    });
+
+    if (clienteEcontrado) {
+
+        let index = clientesArray.indexOf(clienteEcontrado);
+
+        clientesArray.splice(index, 1);
+    }
+
+    res.json(clienteEcontrado);
 });
 
 http.createServer(app).listen(3000, () => console.log("Servidor rodando local na porta 3000"));
